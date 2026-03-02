0b2f4e93dedf90e9887c380d2bfd964f8c02917e
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-03-02 12:02:15 +0100

    Red green refactor singularity of pawn and knight, and not same position

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -4,10 +4,41 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
+    private Logics logic;
 
-  @Test
-  public void testInitialSize() {
-      Logics logic = new LogicsImpl(5);
-      assertEquals(5, logic.getSize());
-  }
+    @BeforeEach
+    public void setUp() {
+        this.logic = new LogicsImpl(5);
+    }
+
+    @Test
+    public void testInitialSize() {
+        assertEquals(5, this.logic.getSize());
+    }
+
+    @Test
+    public void testPawnIsSingle() {
+        int pawnCounter = 0;
+        for (int i = 0; i < this.logic.getSize(); i++) {
+            for (int j = 0; j < this.logic.getSize(); j++) {
+                if (this.logic.hasPawn(i,j)) {
+                    pawnCounter++;
+                }
+            }
+        }
+        assertEquals(1, pawnCounter);
+    }
+
+    @Test
+    public void testKnightIsSingle() {
+        int knightCounter = 0;
+        for (int i = 0; i < this.logic.getSize(); i++) {
+            for (int j = 0; j < this.logic.getSize(); j++) {
+                if (this.logic.hasKnight(i,j)) {
+                    knightCounter++;
+                }
+            }
+        }
+        assertEquals(1, knightCounter);
+    }
 }
