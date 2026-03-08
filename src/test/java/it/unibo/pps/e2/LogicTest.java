f443e302cd0424801e738a91647dee64d0cbc652
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-03-08 11:30:44 +0100

    Tested pawn and knight do not share position

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -5,20 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
     private Logics logic;
+    private static final int LOGIC_INITIAL_SIZE = 5;
 
     @BeforeEach
     public void setUp() {
-        this.logic = new LogicsImpl(5);
+        this.logic = new LogicsImpl(LOGIC_INITIAL_SIZE);
     }
 
     @Test
     public void testInitialSize() {
-        assertEquals(5, this.logic.getSize());
+        assertEquals(LOGIC_INITIAL_SIZE, this.logic.getSize());
     }
 
     @Test
     public void testPawnIsSingle() {
         int pawnCounter = 0;
+        int numberOfPawn = 1;
         for (int i = 0; i < this.logic.getSize(); i++) {
             for (int j = 0; j < this.logic.getSize(); j++) {
                 if (this.logic.hasPawn(i,j)) {
@@ -26,12 +28,13 @@ public class LogicTest {
                 }
             }
         }
-        assertEquals(1, pawnCounter);
+        assertEquals(numberOfPawn, pawnCounter);
     }
 
     @Test
     public void testKnightIsSingle() {
         int knightCounter = 0;
+        int numberOfKnight = 1;
         for (int i = 0; i < this.logic.getSize(); i++) {
             for (int j = 0; j < this.logic.getSize(); j++) {
                 if (this.logic.hasKnight(i,j)) {
@@ -39,6 +42,22 @@ public class LogicTest {
                 }
             }
         }
-        assertEquals(1, knightCounter);
+        assertEquals(numberOfKnight, knightCounter);
+    }
+
+    @Test
+    public void testKnightAndPawnNotInTheSamePosition() {
+        Pair<Integer, Integer> knightPos = null;
+        Pair<Integer, Integer> pawnPos = null;
+        for (int i = 0; i < this.logic.getSize(); i++) {
+            for (int j = 0; j < this.logic.getSize(); j++) {
+                if (this.logic.hasKnight(i,j)) {
+                    knightPos = new Pair<>(i,j);
+                } else if (this.logic.hasPawn(i,j)) {
+                    pawnPos = new Pair<>(i,j);
+                }
+            }
+        }
+        assertNotEquals(knightPos, pawnPos);
     }
 }
